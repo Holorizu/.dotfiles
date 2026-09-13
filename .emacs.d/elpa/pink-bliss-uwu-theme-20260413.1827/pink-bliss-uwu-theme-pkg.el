6aef51aa6e254d495562d3c3707faf65f7f262d7
Holorizu <bmarcus7906@gmail.com>
2026-09-13 15:49:15 +0530

    bruh

--- .emacs.d/elpa/pink-bliss-uwu-theme-20260413.1827/pink-bliss-uwu-theme-pkg.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "pink-bliss-uwu-theme" "20260413.1827"
-  "Pink color theme."
-  '((emacs "24.1"))
-  :url "https://github.com/themkat/pink-bliss-uwu"
-  :commit "26e90134adc997c8f6fbf3820fa9198afa6c832b"
-  :revdesc "26e90134adc9")
